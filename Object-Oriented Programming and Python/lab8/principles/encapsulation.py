10cdf223c300670c51a4178b4030f6af83ec447c
AndyYFTao <48302739+AndyYFTao@users.noreply.github.com>
2019-12-29 14:28:01 +0800

    Add files via upload

---
+++ Object-Oriented Programming and Python/lab8/principles/encapsulation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+class Computer:
+    
+    def __init__(self):
+        self.__maxprice = 900
+    
+    def sell(self):
+        print("Selling Price: {}".format(self.__maxprice))
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
